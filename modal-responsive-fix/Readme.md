53cfd967fa0e140d9da047986cbf67465537e80f
Nick Baugh <niftylettuce@gmail.com>
2012-09-01 04:09:27 -0400

    Added documentation

---
+++ modal-responsive-fix/Readme.md
@@ -0,0 +1,48 @@
+
+# Twitter Bootstrap jQuery Patches - Modal Responsive Fix <sup>0.0.2</sup>
+
+* Resolves Twitter Bootstrap issues [#407 #1017 #1339 #2130 #3361 #3362 #4283][1]
+* Built-in support for fullscreen [Bootstrap Image Gallery][2]
+
+# Quick Start
+
+1. Include [touchscroll.js][3] (fixes mobile scrollable div issue):
+```html
+<script type="text/javascript" src="/js/vendor/touchscroll.min.js"></script>
+```
+2. Include [jquery.modal-responsive-fix.js][4] after the previous `<script>`:
+```html
+<script type="text/javascript" src="/js/vendor/jquery.modal-responsive-fix.min.js"></script>
+```
+3. Patch [modal.less][5] (or [bootstrap.css][6]) (fixes scrollable div heights on mobile):
+```css
+.modal-body {
+  overflow-y: auto;
+  // #L69 of "modal.less"
+  // max-height: 400px; /* remove this */
+  padding: 15px;
+}
+```
+4. **Optional**: Patch [bootstrap-image-gallery.css][7] (only if you are using `.modal-fullscreen` via [Bootstrap Image Gallery][2])
+```css
+// #L137-140
+@media (max-width: 480px) {
+  .modal-gallery .btn span {
+    display: none;
+  }
+  .modal-fullscreen {
+    left: 0 !important;
+    right: 0 !important;
+    margin-top: 0 !important;
+    margin-left: 0 !important;
+  }
+}
+```
+
+[1]: https://github.com/twitter/bootstrap/issues/2130
+[1]: https://github.com/blueimp/Bootstrap-Image-Gallery
+[3]: https://raw.github.com/niftylettuce/twitter-bootstrap-jquery-plugins/master/modal-responsive-fix/touchscroll.js
+[4]: https://raw.github.com/niftylettuce/twitter-bootstrap-jquery-plugins/master/modal-responsive-fix/bootstrap.jquery.modal-responsive-fix.js
+[5]: https://github.com/twitter/bootstrap/blob/master/less/modals.less#L69
+[6]: http://twitter.github.com/bootstrap/assets/css/bootstrap.css
+[7]: https://github.com/blueimp/Bootstrap-Image-Gallery/blob/master/css/bootstrap-image-gallery.css#L137-140
--- modal-responsive-fix/bootstrap.jquery.modal-responsive-fix.js
+++ modal-responsive-fix/bootstrap.jquery.modal-responsive-fix.js
@@ -1,112 +1,73 @@
 
-//     Twitter Bootstrap jQuery Patches - Modal Responsive Fix
+//     Twitter Bootstrap jQuery Plugins - Modal Responsive Fix
 //     Copyright (c) 2012 Nick Baugh <niftylettuce@gmail.com>
 //     MIT Licensed
 //     Version 0.0.2
 
-//
 // * Author: [@niftylettuce](https://twitter.com/#!/niftylettuce)
-// * Source: <https://github.com/niftylettuce/twitter-bootstrap-jquery-patches>
-// * Resolves #407, #1017, #1339, #2130, #3361, #3362, #4283:
-//  <https://github.com/twitter/bootstrap/issues/2130>
-// * Built-in support for fullscreen Bootstrap Image Gallery
-//  <https://github.com/blueimp/Bootstrap-Image-Gallery>
-//
-// **NOTE**: Include "touchscroll.js" before "modal-responsive-fix".
-//  This fixes the mobile issue with Android/iPhone scrollabe `div`'s.
-//  Simply download it from the "Source" link above and add the path:
-//
-//
-// ```html
-// <script type="text/javascript" src="/js/vendor/touchscroll.js"></script>
-// ```
-//
-// **NOTE** You will need to patch "modal.css" as follows:
-//
-// ```css
-// .modal-body {
-//   overflow-y: auto;
-//   // max-height: 400px; /* remove this */
-//   padding: 15px;
-// }
-// ```
-//
-// **NOTE:** If you are using `.modal-fullscreen`, you will need
-//  to add the following CSS to "bootstrap-image-gallery.css":
-//
-// ```js
-// @media (max-width: 480px) {
-//   .modal-fullscreen {
-//     left: 0 !important;
-//     right: 0 !important;
-//     margin-top: 0 !important;
-//     margin-left: 0 !important;
-//   }
-// }
-// ```
-//
+// * Source: <https://github.com/niftylettuce/twitter-bootstrap-jquery-plugins>
 
-// # modal-responsive-fix
+// # bootstrap.jquery.modal-responsive-fix
 
 $(function() {
   // TODO: bind touchScroll to newly created `.modal`'s
-  $('.modal').touchScroll()
-})
+  $('.modal').touchScroll();
+});
 
 var adjustModal = function adjustModal($modal) {
 
-  var top, maxHeight = $(window).height() - 20
+  var top, maxHeight = $(window).height() - 20;
 
   if ($(window).width() <= 480) {
     if ($modal.hasClass('modal-fullscreen')) {
       if ($modal.height() >= $(window).height()) {
-        top = $(window).scrollTop()
+        top = $(window).scrollTop();
       } else {
-        top = $(window).scrollTop() + ($(window).height() - $modal.height()) / 2
+        top = $(window).scrollTop() + ($(window).height() - $modal.height()) / 2;
       }
     } else if ($modal.height() >= $(window).height() - 10) {
-      top = $(window).scrollTop() + 10
+      top = $(window).scrollTop() + 10;
     } else {
-      top = $(window).scrollTop() + ($(window).height() - $modal.height()) / 2
+      top = $(window).scrollTop() + ($(window).height() - $modal.height()) / 2;
     }
   } else {
-    top = '50%'
+    top = '50%';
     if ($modal.hasClass('modal-fullscreen')) {
       $modal.stop().animate({
-          marginTop  : -($modal.outerHeight() / 2)
-        , marginLeft : -($modal.outerWidth() / 2)
-        , top        : top
-        , maxHeight  : maxHeight
-      }, "fast")
-      return
+        marginTop  : -($modal.outerHeight() / 2),
+        marginLeft : -($modal.outerWidth() / 2),
+        top        : top,
+        maxHeight  : maxHeight
+      }, "fast");
+      return;
     }
   }
 
-  $modal.stop().animate({ top: top, maxHeight: maxHeight }, "fast")
+  $modal.stop().animate({ top: top, maxHeight: maxHeight }, "fast");
 
-}
+};
 
 var show = function show() {
-  var $modal = $(this)
-  adjustModal($modal)
-}
+  var $modal = $(this);
+  adjustModal($modal);
+};
 
 var checkShow = function checkShow() {
   $('.modal').each(function() {
-    var $modal = $(this)
-    if ($modal.css('display') !== 'block') return
-    adjustModal($modal)
+    var $modal = $(this);
+    if ($modal.css('display') !== 'block') return;
+    adjustModal($modal);
   });
-}
+};
 
 var modalWindowResize = function modalWindowResize() {
-  $('.modal').not('.modal-gallery').on('show', show)
-  $('.modal-gallery').on('displayed', show)
-  checkShow()
-}
+  $('.modal').not('.modal-gallery').on('show', show);
+  $('.modal-gallery').on('displayed', show);
+  checkShow();
+};
 
-$(modalWindowResize)
+$(modalWindowResize);
 
-$(window).resize(modalWindowResize)
+$(window).resize(modalWindowResize);
 
-$(window).scroll(checkShow)
+$(window).scroll(checkShow);
